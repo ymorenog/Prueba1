4431d20d1e68b58b76aab5653356e02d6cffa475
Yolanda Moreno <yolandamorenog@outlook.com>
2020-11-04 18:35:09 +0100

    xxx

--- src/Clase2.java
+++ src/Clase2.java
@@ -4,11 +4,8 @@ public class Clase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola clase2");
-<<<<<<< HEAD
-=======
-		System.out.println("Hola2 clase2");
 		System.out.println("Hola3 clase2");
->>>>>>> branch 'master' of https://github.com/ymorenog/Prueba1.git
+
 
 	}
 
