ee48cef2a2f939f29e4edf2da0e4bb90c262f208
ymorenog <58043455+ymorenog@users.noreply.github.com>
2020-11-04 17:59:29 +0100

    Update Clase2.java

--- src/Clase2.java
+++ src/Clase2.java
@@ -4,6 +4,7 @@ public class Clase2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola clase2");
+		System.out.println("Hola2 clase2");
 
 	}
 
